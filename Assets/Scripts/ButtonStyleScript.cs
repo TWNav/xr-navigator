977f67417f67b09b4ac1c11e42723eaa4e42555a
Shaun Hubbard <shaun.hubbard@thoughtworks.com>
2020-08-06 17:04:50 -0400

    semi stlyed buttons

---
+++ Assets/Scripts/ButtonStyleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonStyleScript : MonoBehaviour
+{
+    [SerializeField]
+    private Material defaultColor, highlightColor;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        var colorBlock = button.colors;
+        colorBlock.normalColor = defaultColor.color;
+        colorBlock.selectedColor = highlightColor.color;
+        button.colors = colorBlock;
+    }
+}
