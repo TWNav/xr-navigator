b617666e9f9b0f543fa1a6cf0b956af9fdc363f1
Shaun Hubbard <shaun.hubbard@thoughtworks.com>
2020-08-06 15:14:18 -0400

    intial commit

---
+++ Assets/Scripts/TutorialSceneBackgroundScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialSceneBackgroundScript : MonoBehaviour
+{
+    void Update()
+    {
+        // TODO: Make this a click handler
+        if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            SwitchScene();
+        }    
+    }
+
+    private void SwitchScene() {
+        SceneManager.LoadScene("TWNavigatorScene");
+    }
+}
