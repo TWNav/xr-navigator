2e0cf4d7f11659d0e1363f952dcaf0a2f0252a4f
ParisKaman <paris.kaman@thoughtworks.com>
2020-08-07 17:08:55 -0500

    Adds permissions for Android

--- Assets/Scripts/TutorialSceneBackgroundScript.cs
+++ Assets/Scripts/TutorialSceneBackgroundScript.cs
@@ -4,54 +4,67 @@ using System.Threading.Tasks;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#elif UNITY_IOS
+#endif
 
 
 
 
 public class TutorialSceneBackgroundScript : MonoBehaviour
 {
-    private Task locationTask;
-    private bool isLocationEndabled {
+    private bool isLocationEnabled {
         get {
             return Input.location.isEnabledByUser;
         }
     }
     void Start() {
-       locationTask = RequestLocationPermission();
+       RequestPermissions();
     }
     void Update()
     {
-        if(!isLocationEndabled) return;
+        if(!isLocationEnabled) return;
         // TODO: Make this a click handler
         if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began) {
     
             SwitchScene();
         }    
     }
-    public Task RequestLocationPermission(){
-        var t = new Task( () => {
-        if(isLocationEndabled)
-        {
 
-            Debug.Log("Location enabled.");
-            return;
-        }
-        Debug.Log("Location not endabled.");
-        Input.location.Start();
-        Debug.Log("Location requested.");
-        while(Input.location.status == LocationServiceStatus.Initializing)
-        {
-
-        }
-
-        Debug.Log($"Location request completed status: {isLocationEndabled}");
-        locationTask = null;
-    });
-        t.Start();
-        return t;
+    private void RequestPermissions()
+    {
+        #if UNITY_ANDROID
+        RequestPermissionIfNotGiven(Permission.FineLocation);
+        RequestPermissionIfNotGiven(Permission.Camera);
+        #elif UNITY_IOS
+        //TODO: Implement permissions for iOS
+        Debug.Log("Request Permissions iOS");
+        #endif
     }
 
+#if UNITY_ANDROID
+    private void RequestPermissionIfNotGiven(string permission)
+    {
+        if(!Permission.HasUserAuthorizedPermission(permission))
+        {
+            Debug.Log($"Requesting access to {permission}");
+            Permission.RequestUserPermission(permission);
+            //TODO: Find a better way to recognize when permission has been granted
+            while(!Permission.HasUserAuthorizedPermission(permission))
+            {
+                Thread.Sleep(500);
+            }
+            Debug.Log(String.Format("Permission for {0} is {1}", permission, Permission.HasUserAuthorizedPermission(permission)));
+        }
+        else
+        {
+            Debug.Log($"Already had permission to {permission}");
+        }
+    }
+#endif
     private void SwitchScene() {
         SceneManager.LoadScene("TWNavigatorScene");
     }
