63bb4ccdae038f24f207ea3bbc134eb92d2be9d9
chenmli2020 <35786460+chenmli@users.noreply.github.com>
2020-08-07 15:57:56 -0500

    adds permissions

--- Assets/Scripts/TutorialSceneBackgroundScript.cs
+++ Assets/Scripts/TutorialSceneBackgroundScript.cs
@@ -1,16 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
+
+
 public class TutorialSceneBackgroundScript : MonoBehaviour
 {
+    private Task locationTask;
+    private bool isLocationEndabled {
+        get {
+            return Input.location.isEnabledByUser;
+        }
+    }
+    void Start() {
+       locationTask = RequestLocationPermission();
+    }
     void Update()
     {
+        if(!isLocationEndabled) return;
         // TODO: Make this a click handler
         if(Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began) {
+    
             SwitchScene();
         }    
+    }
+    public Task RequestLocationPermission(){
+        var t = new Task( () => {
+        if(isLocationEndabled)
+        {
+
+            Debug.Log("Location enabled.");
+            return;
+        }
+        Debug.Log("Location not endabled.");
+        Input.location.Start();
+        Debug.Log("Location requested.");
+        while(Input.location.status == LocationServiceStatus.Initializing)
+        {
+
+        }
+
+        Debug.Log($"Location request completed status: {isLocationEndabled}");
+        locationTask = null;
+    });
+        t.Start();
+        return t;
     }
 
     private void SwitchScene() {
