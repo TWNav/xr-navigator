ab68bc71cb09389979cc25828af16d62351d2b54
ParisKaman <paris.kaman@thoughtworks.com>
2020-08-18 14:36:36 -0500

    Adds sandbox scene for anchor selection

---
+++ Assets/Scripts/AnchorLerper.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AnchorLerper : MonoBehaviour
+{
+
+    private Transform anchorContainerTransform;
+    [SerializeField]
+    private Transform cameraContainerTransform;
+
+    private Transform currentRenderTransform;
+
+    public GameObject anchorToLerp;
+
+    public GameObject renderToLerp;
+    private Vector3 startingScale, targetScale;
+    private Vector3 selectedAnchorOriginalScale;
+
+    public float timeToFocus;
+    private float currentLerpTime = 0;
+
+    private bool inAnchorContainer = true;
+
+    private bool lerpingFromContainerToCamera = false;
+    private bool lerpingFromCameraToContainer = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lerpingFromContainerToCamera)
+        {
+            lerpingFromContainerToCamera = !LerpToTransform(currentRenderTransform.transform, anchorContainerTransform.transform, cameraContainerTransform.transform);
+        }
+        else if (lerpingFromCameraToContainer)
+        {
+            lerpingFromCameraToContainer = !LerpToTransform(currentRenderTransform.transform, cameraContainerTransform.transform, anchorContainerTransform.transform);
+        }
+    }
+
+    public bool LerpToTransform(Transform objectToLerp, Transform originTransform, Transform destinationTransform)
+    {
+        bool lerpComplete = true;
+
+        currentLerpTime += Time.deltaTime;
+        if(currentLerpTime > timeToFocus)
+        {
+            currentLerpTime = timeToFocus;
+        }
+
+        if(Vector3.Distance(objectToLerp.position, destinationTransform.position) > 0.001f)
+        {
+            objectToLerp.position = Vector3.Lerp(originTransform.position, destinationTransform.position, currentLerpTime / timeToFocus);
+            objectToLerp.rotation = Quaternion.Lerp( originTransform.rotation, destinationTransform.rotation, currentLerpTime / timeToFocus);
+            objectToLerp.localScale = Vector3.Lerp(startingScale, targetScale, currentLerpTime / timeToFocus);
+            lerpComplete = false;
+        }
+        else{
+            Debug.Log("Resetting currentLerpTime");
+            currentLerpTime = 0;
+            if(!inAnchorContainer)
+            {
+                StopRotation();
+            }
+        }
+
+        return lerpComplete;
+    }
+
+    private void StopRotation()
+    {
+        renderToLerp.GetComponentInChildren<RotateCircle>().enabled = false;
+        Vector3 textRotation = renderToLerp.GetComponentInChildren<RotateCircle>().gameObject.transform.rotation.eulerAngles;
+        Debug.Log($"{textRotation}");
+        renderToLerp.GetComponentInChildren<RotateCircle>().gameObject.transform.rotation = Quaternion.Euler(textRotation.x, textRotation.y, 0f);
+    }
+
+
+    public void SelectAnchor()
+    {
+        if(inAnchorContainer && !(lerpingFromCameraToContainer || lerpingFromContainerToCamera))
+        {
+            anchorContainerTransform = anchorToLerp.transform;
+            selectedAnchorOriginalScale = renderToLerp.transform.localScale;
+
+
+            renderToLerp.transform.SetParent(cameraContainerTransform);
+            currentRenderTransform = renderToLerp.transform;
+            targetScale = new Vector3(1,1,1);
+            startingScale = renderToLerp.transform.localScale;
+            lerpingFromCameraToContainer = false;
+            lerpingFromContainerToCamera = true;
+            renderToLerp.GetComponentInChildren<TMP_Text>().enabled = false;
+            renderToLerp.GetComponentInChildren<TMP_Text>().enabled = true;
+            inAnchorContainer = false;
+        }
+    }
+
+    public void SubmitAnchor()
+    {
+        if(!inAnchorContainer && !(lerpingFromCameraToContainer || lerpingFromContainerToCamera))
+        {
+            renderToLerp.transform.SetParent(anchorContainerTransform);
+            currentRenderTransform = renderToLerp.transform;
+            targetScale = selectedAnchorOriginalScale;
+            startingScale = renderToLerp.transform.localScale;
+            lerpingFromContainerToCamera = false;
+            lerpingFromCameraToContainer = true;
+            renderToLerp.GetComponentInChildren<TMP_Text>().enabled = false;
+            renderToLerp.GetComponentInChildren<TMP_Text>().enabled = true;
+            inAnchorContainer = true;
+            renderToLerp.GetComponentInChildren<RotateCircle>().enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/RenameAnchorHandler.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using ntw.CurvedTextMeshPro;
+
+public class RenameAnchorHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    private InputField inputField;
+
+    [SerializeField]
+    private GameObject objectToRename;
+
+    private TMP_Text circleMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField.characterLimit = 28;
+        circleMesh = objectToRename.GetComponentInChildren<TMP_Text>();
+    }
+
+    void Update()
+    {
+        string anchorText = inputField.text;
+        if(inputField.text.Length == 0)
+        {
+            circleMesh.text = "TEST   TEST    TEST   TEST";
+        }
+        else
+        {
+            GenerateLabel();
+        }
+    }
+
+    private void GenerateLabel()
+    {
+        string label = inputField.text;
+        int anchorLabelLength = 0;
+        circleMesh.text = "";
+        while(anchorLabelLength <= 28)
+        {
+            circleMesh.text += label;
+            int spaceAmount = 0;
+            for(int i = 0; i <= label.Length; i+=4)
+            {
+                spaceAmount++;
+                circleMesh.text += " ";
+            }
+            if(label.Length >= 9 && label.Length <= 11)
+            {
+                circleMesh.GetComponent<TextProOnACircle>().m_arcDegrees = 320;
+            }
+            else{
+                circleMesh.GetComponent<TextProOnACircle>().m_arcDegrees = 340;
+            }
+            anchorLabelLength += label.Length + spaceAmount;
+            if(anchorLabelLength + label.Length > 28)
+            {
+                return;
+            }
+        }
+    }
+
+    public void SelectInputField()
+    {
+        inputField.ActivateInputField();
+        inputField.Select();
+    }
+
+    public void DeselectInputField()
+    {
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void SubmitAnchorUpdate()
+    {
+        
+    }
+}
