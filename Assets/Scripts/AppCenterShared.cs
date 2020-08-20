4de74144a2b77a1e79c67cc8fe4a28b447d8e45a
Shaun Hubbard <shaun.hubbard@thoughtworks.com>
2020-08-20 12:26:52 -0400

    shared app center behavior:

---
+++ Assets/Scripts/AppCenterShared.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppCenterShared : MonoBehaviour
+{
+    AppCenterBehavior appCenter = null;
+    private void Start() {
+        Debug.Log("Should find AppCenterBehavior.");
+        
+        appCenter = FindObjectOfType<AppCenterBehavior>();
+        if (appCenter) {
+            Debug.Log("Did find AppCenterBehavior.");
+        } else {
+            Debug.Log("Did NOT find AppCenterBehavior.");
+        }
+    }
+
+}
